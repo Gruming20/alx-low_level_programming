4ebc6001e6ec653dbaab96f05dc80bd7025c0780
Gruming20 <gruming.898@gmail.com>
2022-07-06 12:24:59 -0700

    1 alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints "alphabets"
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+_putchar(low);
+_putchar('\n');
+}
