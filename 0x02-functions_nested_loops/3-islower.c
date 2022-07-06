4011dc1f78413b0c3ebe47eb9dc912bd17dcc302
Gruming20 <gruming.898@gmail.com>
2022-07-06 13:21:28 -0700

    3 is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - prints "011"
- *
- * Return: nothing
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
