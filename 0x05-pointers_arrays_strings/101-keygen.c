b84e11a352b39e28e842a75193efb47a3913bfe6
Gruming20 <gruming.898@gmail.com>
2022-07-12 12:04:23 -0700

    101 keygen for 101 crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,8 +16,9 @@ int random = 0;
 srand((unsigned int)(time(NULL)));
 
 char *num;
+char *letter;
 num = "0123456789";
-char letter[] = "abcdefghijklmnoqprstuvwyzx";
+letter = "abcdefghijklmnoqprstuvwyzx";
 char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 char symbols[] = "!@#$^&*?";
 char password[10];
@@ -27,27 +28,27 @@ for (i = 0; i < N; i++)
 {
 if (random == 1)
 {
-password[i] = num[rand() % 10];
+password[N] = num[rand() % 10];
 random = rand() % 4;
-printf("%c", password[i]);
+printf("%c", password[N]);
 }
 else if (random == 2)
 {
-password[i] = symbols[rand() % 8];
+password[N] = symbols[rand() % 8];
 random = rand() % 4;
-printf("%c", password[i]);
+printf("%c", password[N]);
 }
 else if (random == 3)
 {
-password[i] = LETTER[rand() % 26];
+password[N] = LETTER[rand() % 26];
 random = rand() % 4;
-printf("%c", password[i]);
+printf("%c", password[N]);
 }
 else
 {
-password[i] = letter[rand() % 26];
+password[N] = letter[rand() % 26];
 random = rand() % 4;
-printf("%c", password[i]);
+printf("%c", password[N]);
 }
 }
 }
